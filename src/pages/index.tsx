10761a1ff859cc5c57402eb5992a2a4bfd375120
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-08-19 21:31:00 +0100

    js files changed to tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen bg-blue-500">
-      <h1 className="text-3xs">Testandooo</h1>
+      <h1 className="text-3xl">Testandooo</h1>
     </div>
   );
 }
