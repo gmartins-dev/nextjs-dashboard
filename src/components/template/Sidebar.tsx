d687aa4d6850884a5d395a81d88cca90e9347389
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-08-21 21:14:03 +0100

    sidebar updates

--- src/components/template/Sidebar.tsx
+++ src/components/template/Sidebar.tsx
@@ -1,20 +1,28 @@
-import { IconAdjustments, IconBell, IconHome } from "../icons/index";
+import { IconAdjustments, IconBell, IconHome, IconLogout } from "../icons/index";
 import Logo from "./Logo";
 import SidebarItem from "./SidebarItem";
 
 export default function Sidebar() {
 
   return(
-    <aside>
+    <aside className="flex flex-col">
       <div className="flex flex-col items-center justify-center w-20 h-20 bg-gradient-to-r from-indigo-500 via-blue-600 to-purple-800">
         <Logo/>
       </div>
-      <ul>
+      <ul className="flex-grow">
         <SidebarItem url="/" text="Home" icon={IconHome}/>
 
         <SidebarItem url="/adjustments" text="Configs" icon={IconAdjustments}/>
 
         <SidebarItem url="/notifications" text="Notifications" icon={IconBell}/>
+      </ul>
+      <ul>
+        <SidebarItem 
+        text="Logout" 
+        icon={IconLogout} 
+        onClick={() => console.log("logoooouuutt")}
+        className={`text-red-600 hover:bg-red-400 hover:text-white`}
+        />
       </ul>
     </aside>
   )
--- src/components/template/SidebarItem.tsx
+++ src/components/template/SidebarItem.tsx
@@ -1,24 +1,36 @@
 import Link from "../../../node_modules/next/link"
 
 interface SidebarItemProps {
-  url: string,
+  url?: string,
   text: string,
-  icon: any
+  icon: any,
+  className?: string, 
+  onClick?: (event: any) => void
 }
  
 export default function SidebarItem(props: SidebarItemProps) {
+  
+  function linkRender(){
+    return (
+      <a 
+      className={`flex flex-col items-center justify-center w-20 h-20 text-gray-600 ${props.className}`}>
+      {props.icon}
+      <span 
+      className="w-20 text-xs font-light text-center">
+        {props.text}
+      </span>
+      </a>
+    )
+  }
+  
   return (
-    <li className="hover:bg-gray-100">
-      <Link href={props.url}>
-        <a 
-        className="flex flex-col items-center justify-center w-full h-20">
-        {props.icon}
-        <span 
-        className="w-20 text-xs font-light text-center text-gray-600">
-          {props.text}
-        </span>
-        </a>
-      </Link>
+    <li onClick={props.onClick} className="cursor-pointer hover:bg-gray-100">
+      {props.url ? (
+        <Link href={props.url}>
+         {linkRender()}
+        </Link>
+        ):(linkRender()
+      )}
     </li>
   )
 }
