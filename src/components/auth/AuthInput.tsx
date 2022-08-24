31cd3c0d20d2cdc2a4c9514fa4868c7bd52e1ece
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-08-24 10:37:14 +0100

    form login created

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -10,13 +10,14 @@ interface AuthInputProps {
 export default function AuthInput(props: AuthInputProps){
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col mt-4">
       <label>{props.label}</label>
       <input 
       type={props.type ?? 'text'}
       value={props.value}
       required={props.required}
       onChange={event => props.newValue?.(event.target.value)}
+      className="px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:outline-none focus:bg-white"
       />
     </div>
   )
--- src/pages/authentication.tsx
+++ src/pages/authentication.tsx
@@ -7,23 +7,43 @@ export default function Authentication(){
   const [password, setPassword] = useState('')
   const [status, setStatus] = useState<'logged' | 'register'>('logged')
 
+  function handleSubmit() {
+    if(status === 'logged'){
+      console.log("loginnn")
+    } else{
+      console.log("registerrrr")
+    }
+  }
+
   return (
-    <div>
-      <h1>authhhh</h1>
-      <AuthInput
-        label="Email"
-        type="email"
-        value={email}
-        newValue={setEmail}
-        required={true}
-      />
+    <div className="flex flex-col h-screen items-center justify-center">
+      <div className="w-1/2">
+        <h1 className="text-xl font-bold mb-5">
+          {status === 'logged' ? "Please login to enter" : "Please register your account"}
+        </h1>
         <AuthInput
-        label="Password"
-        type="password"
-        value={password}
-        required={true}
-        newValue={setPassword}
-      />
+          label="Email"
+          type="email"
+          value={email}
+          newValue={setEmail}
+          required={true}
+        />
+          <AuthInput
+          label="Password"
+          type="password"
+          value={password}
+          required={true}
+          newValue={setPassword}
+        />
+
+        <button onClick={handleSubmit} className='w-full bg-indigo-500 hover:bg-indigo-400 text-white rounded-lg px-4 py-3 mt-6'>
+          {status === 'logged' ? "Enter" : "Register"}
+        </button>
+        <hr className="my-6 border-gray-300 w-full"/>
+        <button onClick={handleSubmit} className='w-full bg-red-500 hover:bg-red-400 text-white rounded-lg px-4 py-3'>
+          {status === 'logged' ? "Enter with Google" : "Register with Google"}
+        </button>
+      </div>
     </div>
   )
 }
