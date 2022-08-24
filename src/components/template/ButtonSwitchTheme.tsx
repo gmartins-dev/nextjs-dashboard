45c33756f5b0ab9f54b3048099ded2bf81bcfc24
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-08-24 08:49:59 +0100

    added style to ButtonSwitchTheme

--- src/components/template/ButtonSwitchTheme.tsx
+++ src/components/template/ButtonSwitchTheme.tsx
@@ -9,23 +9,23 @@ interface ButtonSwitchThemeProps {
 export default function ButtonSwitchTheme(props: ButtonSwitchThemeProps){
   return(
     props.theme === 'dark' ? (
-      <div onClick={props.switchTheme}>
-        <div className="">
-          {IconSun}
+      <div onClick={props.switchTheme} className="hidden sm:flex bg-gradient-to-r from-yellow-300 to-yellow-600 w-14 lg:w-24 h-8 items-center cursor-pointer p-1 rounded-full">
+        <div className="flex items-center justify-center bg-white text-yellow-600 w-6 h-6 rounded-full" >
+          {IconSun(4)}
         </div>
-        <div className="">
-          <span>Light</span>
+        <div className="hidden lg:flex items-center ml-4 text-white">
+        <span className="text-sm">Light</span>
         </div>
-      </div>
+        </div>
     ):(
-      <div onClick={props.switchTheme}>
-      <div>
-        {IconMoon}
+      <div onClick={props.switchTheme} className="hidden sm:flex bg-gradient-to-r from-gray-500 to-gray-900 w-14 lg:w-24 h-8 items-center justify-end cursor-pointer p-1 rounded-full">
+        <div className="hidden lg:flex items-center mr-2 text-gray-300">
+         <span className="text-sm">Dark</span>
+        </div>
+        <div className="flex items-center justify-center bg-black text-yellow-300 w-6 h-6 rounded-full" >
+          {IconMoon(4)}
+        </div>
       </div>
-      <div>
-        <span>Dark</span>
-      </div>
-    </div>
     )
   )
 }
